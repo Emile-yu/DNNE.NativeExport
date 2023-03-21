946582ae88d141de7e2161f72d052a4e92114da3
YU Jiangen <yjg9091@gmail.com>
2023-03-21 18:09:06 +0100

    Init Commit

---
+++ ClassLibrary1/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+#if NETFRAMEWORK
+    [DNNE.Export(EntryPoint = "IntIntInt")]
+#else
+       
+     [UnmanagedCallersOnly(CallConvs = new[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+#endif
+
+        public static Int64 UnmanagedIntIntInt(Int64 a, Int64 b)
+        {
+            return a + b;
+        }
+
+        [UnmanagedCallersOnly(CallConvs = new[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+        public static bool Test()
+        {
+            return true;
+        }
+    }
+}
+
+#if NETFRAMEWORK
+namespace DNNE
+{
+    internal class ExportAttribute : Attribute
+    {
+        public ExportAttribute()
+        {
+
+        }
+
+        public string EntryPoint { get; set; }
+    }
+}
+#endif
